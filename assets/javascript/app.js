4eab9dd8b0609c5440a93ba8d7ae31ee4bc8cd18
murnda <mdelong333@gmail.com>
2019-09-26 17:08:35 -0400

    added initial queryURL

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,3 @@
 var topics = ["creepy", "cross-stitch", "weird", "painting", "drawing", "piercings", "tattoos", "cats", "tarot", "spooky", "halloween"];
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cvRWJMp97Is61OrbMFOy1QvO6ypDnp7Y";
