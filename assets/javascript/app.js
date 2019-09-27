b96d7c58c028e66889e327f0aa364a346297b121
murnda <mdelong333@gmail.com>
2019-09-27 17:16:24 -0400

    static gif images and their ratings show on page when button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,57 @@
-var topics = ["creepy", "cross-stitch", "weird", "painting", "drawing", "piercings", "tattoos", "cats", "tarot", "spooky", "halloween"];
+$(document).ready(function() {
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cvRWJMp97Is61OrbMFOy1QvO6ypDnp7Y";
+    //topics array for buttons
+    var topics = ["creepy", "cross-stitch", "weird", "painting", "drawing", "piercings", "tattoos", "cats", "tarot", "spooky", "halloween"];
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
+    //calls function to create a button for each topic
+    makeButtons();
 
-});
+    //function runs when a button is clicked
+    $("button").on("click", function() {
 
+        //variable to hold data for the button that was clicked
+        var topic = $(this).attr("data-topic");
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cvRWJMp97Is61OrbMFOy1QvO6ypDnp7Y&q=" + topic + "&limit=10&lang=en";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+
+            var results = response.data;
+            
+            //loops through results of query associated with the button clicked displays static gif image and rating in gif-display div
+            for (var r = 0; r < results.length; r++) {
+                
+                var rating = results[r].rating;
+
+                $("#gif-display").append(`<img src="${results[r].images.fixed_height_still.url}">
+                <p>Rating: ${rating}</p>`)
+            };
+
+            //console log query url and data response
+            console.log(queryURL);
+            console.log(response.data);
+        });
+
+    })
+    
+    //loops through topics array and adds buttons to buttons div
+    function makeButtons() {
+        for (var t = 0; t < topics.length; t++) {
+            $("#buttons").append(`<button class="topicButton" data-topic="${topics[t]}">${topics[t]}</button>
+            `)    
+            
+        }
+    };
 
     
-//loops through topics var and adds buttons to buttons div
-for (var t = 0; t < topics.length; t++) {
-    $("#buttons").append(`<button class="topicButton" id="${topics[t]}">${topics[t]}</button>
-    `)    
-    console.log(topics[t]);
-    }
+    
+        
+    
+})
 
-
-
-//take in array and output buttons of each string
 //when button is clicked add 10 static gifs from GIPHY API to screen
 //when gif is clicked start playing - when clicked again stop playing
 //under each gif display it's rating
